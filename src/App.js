c36f3ee26da77e7aba4998a036c7dca53b8deef8
pariosa <ariosa@gmail.com>
2018-10-17 15:57:46 -0400

    style the button and header of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 import logo from './logo.svg';
 import store from './js/store/index';
 import './App.css'; 
-import {increment,decrement} from './js/actions'
+import {increment,decrement} from './js/actions';
 import  { connect } from 'react-redux';
+const Title = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  color: palevioletred;
+`;
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  &:hover {
+    background: palevioletred;
+    color:white;
+  }
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
 class App extends Component {
 
   incrementCounter = () => {
@@ -13,19 +41,21 @@ class App extends Component {
   decrementCounter = () => {
     store.dispatch(decrement(1)); 
   }  
-  
+
   render() {
     return (
-      <div className="App">
-        <body>
-          <h1>Counter: { this.props.count }</h1> 
-          <button onClick= { this.decrementCounter }> -1 (decrement)</button>
-          <button onClick= { this.incrementCounter }> +1 (increment)</button>
-        </body>
-      </div>
+      <Wrapper>
+        <div className="App">
+          <body>
+            <Title>Counter: { this.props.count }</Title> 
+            <Button onClick= { this.decrementCounter }> -1 (decrement)</Button>
+            <Button onClick= { this.incrementCounter }> +1 (increment)</Button>
+          </body>
+        </div>
+      </Wrapper>
     );
   }
-}
+} 
 
 const mapStateToProps = (state) => {
     return {
